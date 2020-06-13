d2f49957e3cdd86ca7800fe8477021e7515e8470
Ryan <mhogar98@gmail.com>
2020-06-13 12:34:06 -0400

    #5 - Convert migration functions in methods
    - create new struct to encapsulate dependencies and methods
    - make chainError unexported
    - add godoc link to readme

--- helpers.go
+++ helpers.go
@@ -2,7 +2,6 @@ package migrationrunner
 
 import "errors"
 
-// ChainError will combine the error message and the message together in an easy to read manner.
-func ChainError(message string, err error) error {
+func chainError(message string, err error) error {
 	return errors.New(message + "\n\t" + err.Error())
 }
