0216dd5744e9abb405f09bc37901e2e2c42a5a8d
Ryan <mhogar98@gmail.com>
2021-08-19 00:57:07 -0400

    #4 - Fill in README and update package documentation

--- migration.go
+++ migration.go
@@ -3,10 +3,7 @@ package migrationrunner
 // Migration is a struct that represents a single migration.
 type Migration struct {
 	// The required timestamp string for the migration.
-	// The format is flexible, but must be orderable using standard string comparison.
-	//
-	// eg. "20200628151601".
-	// This reads as 2020, June 28, at 15:16, migration #1.
+	// The format is arbitrary but must be orderable using standard string comparison.
 	Timestamp string
 
 	// An optional string that will be displayed alongside the timestamp in logs.
@@ -17,6 +14,9 @@ type Migration struct {
 }
 
 // Migrator is an interface for running a migration.
+//
+// A new struct that implements this interface should be created for each migration.
+// Any additional dependencies required to run your migrations can be added as fields to this struct.
 type Migrator interface {
 	// Up runs the migration and returns any errors.
 	Up() error
